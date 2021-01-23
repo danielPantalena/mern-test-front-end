a79edc51bc7e225bc98bdf54645b704425e899e4
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-23 17:38:43 -0300

    Integrate AccountPage with back end

--- src/Screens/Account.tsx
+++ src/Screens/Account.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { getAccount } from '../services/api';
 
 interface IParams {
   name: string;
 }
 
+interface IAccount {
+  name: string;
+  _id: string;
+}
+
 const Account: React.FC = () => {
-  const { name } = useParams<IParams>();
+  const { name: nameParam } = useParams<IParams>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [accountData, setAccountData] = useState<IAccount | null>(null);
+
+  useEffect(() => {
+    getAccount(nameParam).then((accountResponse) => {
+      if (!accountResponse) setAccountData(null);
+      else setAccountData(accountResponse);
+      return setIsLoading(false);
+    });
+  }, [nameParam]);
+
+  if (isLoading) return <h1>loading...</h1>;
+
+  if (!accountData) return <h1>Account not found</h1>;
 
   return (
     <div>
-      <h1>This page is for {name}</h1>
+      <h3>This page is for <h1>{accountData.name}</h1> the owner of the id {accountData._id}</h3>
       <button>SHARE</button>
     </div>
   );
--- src/services/api.ts
+++ src/services/api.ts
@@ -8,10 +8,14 @@ const baseURL = process.env.REACT_APP_SERVER ?? 'http://localhost:3333';
 const instance = axios.create({
   baseURL,
   headers: { 'Content-type': 'application/json' },
+  timeout: 3000
 });
 
 export const getAccount = async (name: string) =>
-  instance.get(`/${name}`).then((response) => console.log(response));
+  instance.get(`/${name}`).then(
+    (response) => response.data,
+    (_err) => null,
+  );
 
 export const createAccount = async (name: string) =>
   instance.post('/', { name }).then((response) => console.log(response));
