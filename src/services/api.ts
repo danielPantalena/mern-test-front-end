9ec68e050702c25c7415a3e3305b436cd4faed4e
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-23 15:19:36 -0300

    Create axios api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,12 @@
-import axios from 'axios'
+import axios from 'axios';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
+const baseURL = process.env.REACT_APP_SERVER ?? 'http://localhost:3333';
 
 const api = axios.create({
-  
-})
+  baseURL,
+  timeout: 5000,
+  headers: { 'Content-type': 'application/json' },
+});
