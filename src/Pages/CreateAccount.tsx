192765a5b22553881125172540e3e76b232f96fc
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-23 18:10:01 -0300

    Integrate CreateAccount page with back end

--- src/Pages/CreateAccount.tsx
+++ src/Pages/CreateAccount.tsx
@@ -1,12 +1,32 @@
 import React, { useState } from 'react';
+import { createAccount } from '../services/api';
 
 const CreateAccount: React.FC = () => {
-  const [accountName, setAccountName] = useState('');
+  const [accountName, setAccountName] = useState<string | undefined>('');
+  const [message, setMessage] = useState('');
+
+  const handleNameInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage('');
+    setAccountName(event?.target?.value);
+  };
+
+  const handleCreateAccountClick = async () => {
+    if (!accountName) return null;
+    createAccount(accountName).then((createdAccount) => {
+      if (!createdAccount) {
+        setMessage('This account already exists');
+      } else {
+        setMessage(`Account created: ${JSON.stringify(createdAccount)}`);
+      }
+      setAccountName('');
+    });
+  };
 
   return (
     <div>
-      <input onChange={(event) => setAccountName(event.target.value)} />
-      <button>Create account</button>
+      <input value={accountName} onChange={handleNameInput} />
+      <button onClick={handleCreateAccountClick}>Create account</button>
+      <p>{message}</p>
     </div>
   );
 };
--- src/services/api.ts
+++ src/services/api.ts
@@ -8,7 +8,7 @@ const baseURL = process.env.REACT_APP_SERVER ?? 'http://localhost:3333';
 const instance = axios.create({
   baseURL,
   headers: { 'Content-type': 'application/json' },
-  timeout: 3000
+  timeout: 3000,
 });
 
 export const getAccount = async (name: string) =>
@@ -18,4 +18,7 @@ export const getAccount = async (name: string) =>
   );
 
 export const createAccount = async (name: string) =>
-  instance.post('/', { name }).then((response) => console.log(response));
+  instance.post('/admin', { name }).then(
+    (response) => response.data,
+    (_err) => null,
+  );
