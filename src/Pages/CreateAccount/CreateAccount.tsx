7739253783ee2db78190b3dcfa8ed0483899194f
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-25 08:56:46 -0300

    Add generatePage function

--- src/Pages/CreateAccount/CreateAccount.tsx
+++ src/Pages/CreateAccount/CreateAccount.tsx
@@ -4,29 +4,29 @@ import { createAccount } from '../../services/api';
 import './style.css';
 
 const CreateAccount: React.FC = () => {
-  const [accountName, setAccountName] = useState<string | undefined>('');
+  const [accountName, setAccountName] = useState('');
   const [message, setMessage] = useState('');
 
   const handleNameInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setMessage('');
-    setAccountName(event?.target?.value);
+    setAccountName(event?.target?.value ?? '');
+  };
+
+  const generatePage = (name: string = accountName) => {
+    window.open(name, '_blank');
   };
 
   const handleCreateAccountClick = async () => {
     if (!accountName) return setMessage('Please insert a name');
     createAccount(accountName).then((createdAccount) => {
-      setMessage(
-        createdAccount
-          ? `Account created: ${JSON.stringify(createdAccount)}`
-          : 'Account already exists',
-      );
+      if (!createdAccount) setMessage('Account already exists');
+      else generatePage(createdAccount.name);
       setAccountName('');
     });
   };
 
   return (
     <div className="flex-column-container">
-      <h1>Create an Account</h1>
       <input className="name-input" value={accountName} onChange={handleNameInput} />
       <button onClick={handleCreateAccountClick}>Create account</button>
       <p>{message}</p>
